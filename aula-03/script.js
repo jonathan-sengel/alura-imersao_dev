423e2d6ca7c4f0eda6d157b92f54564790bac6ea
Jonathan Engel <joow.engeel@gmail.com>
2021-03-31 09:54:35 -0300

    quase finalizado aula 03 mentalista

--- aula-03/script.js
+++ aula-03/script.js
@@ -1,9 +1,11 @@
 let coins = 3;
 const $colors = document.querySelectorAll('.colors');
 const $resultH2 = document.querySelector('#result');
+const $textH2 = document.querySelector('#info');
 const $restartBtn = document.querySelector('#restart');
 let randomMove = Math.floor(Math.random() * 10);
 let win = false;
+let correctColorElement = $colors[randomMove];
 
 $colors.forEach((color, index) => {
     color.addEventListener('click', validateColor);
@@ -14,21 +16,23 @@ $restartBtn.addEventListener('click', restartGame);
 
 function validateColor(e) {
     const elementClicked = e.target;
+    let correctColor = window.getComputedStyle(correctColorElement, null).getPropertyValue('background-color');
 
     if (coins > 0 && !win) {
         if (elementClicked.dataset.id == randomMove) {
             elementClicked.classList.add('correct');
             win = true;
-            $resultH2.innerText = `Parabéns, voce acertou!`
+            $resultH2.innerText = `Parabéns, voce acertou!`;
+            document.body.style.backgroundColor = correctColor;
+            changeStyleH2Win()
         } else {
             elementClicked.classList.add('wrong');
             coins -= 1;
             $resultH2.innerText = `Jogadas restantes: ${coins}`
         }
         if (coins <= 0) {
-            let correctColor = window.getComputedStyle($colors[randomMove], null).getPropertyValue('background-color');
-            $resultH2.innerText = `Suas tentativas acabaram, a cor correta era ${correctColor}`
-            document.body.style.backgroundColor = correctColor;
+            $resultH2.innerHTML = `Suas tentativas acabaram, a cor correta era <br><mark style="background-color:${correctColor};">${correctColor}</mark>`;
+            correctColorElement.classList.add('correct-color');
         }
     } else {
         console.log('jogo já finalizado');
@@ -39,10 +43,21 @@ function restartGame() {
     coins = 3;
     randomMove = Math.floor(Math.random() * 10);
     win = false;
-    $colors.forEach(color =>{
+    correctColorElement = $colors[randomMove];
+    $colors.forEach(color => {
         color.classList.remove('correct');
         color.classList.remove('wrong');
-        document.body.style.backgroundColor = '#000814e1';
-        $resultH2.innerText = `Jogadas restantes: ${coins}`;
+        color.classList.remove('correct-color');
     })
+    document.body.style.backgroundColor = '#000814e1';
+    $resultH2.innerText = `Jogadas restantes: ${coins}`;
+    changeStyleH2Restart()
+}
+
+function changeStyleH2Win() {
+    $resultH2.classList.add('finished');
+}
+
+function changeStyleH2Restart() {
+    $resultH2.classList.remove('finished');
 }
