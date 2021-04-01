91b13344e1d59d22e99480b21d05064d5938fc8a
Jonathan Engel <joow.engeel@gmail.com>
2021-04-01 20:46:54 -0300

    quase finalizada aula 05

--- aula-05/search.js
+++ aula-05/search.js
@@ -1,57 +1,82 @@
-'use strict'
+let $modalBtnAdd = document.querySelector('input[type="submit"]');
+$modalBtnAdd.addEventListener('click', addMovie);
 
-const createSearchUrl = function (query) {
-    return 'https://api.themoviedb.org/3/search/movie?api_key=eda6d9a6a9d492a2c3205744b78a5d19&language=pt-BR&query=' + query + '&page=1&include_adult=false';
+let $personalBtnAdd = document.querySelector('.add-movie');
+$personalBtnAdd.addEventListener('click', showModal);
+
+function createFavoriteMovie(name, trailerLink, imageUrl, review) {
+
+    const movie = {};
+    movie.name = name;
+    movie.link = trailerLink;
+    movie.image = imageUrl;
+    movie.review = review;
+    return movie;
 }
 
-let $search = document.querySelector('.search-input');
-let $resultList = document.querySelector('.result-list');
-let $btnAdd = document.querySelector('.btn-add-movie');
+function showMovieOnPage(movie) {
 
-let resultMovies = [];
+    let div1 = document.createElement('div');
+    let divMedia = document.createElement('div');
+    let imgMedia = document.createElement('img');
 
-$search.addEventListener('keyup', valideKey);
-$btnAdd.addEventListener('click', uploadMovies);
+    div1.classList.add('movies-list__movie');
+    divMedia.classList.add('movie__media');
+    imgMedia.classList.add('movie__media__poster');
 
-async function loadSearch(query) {
-    let url = createSearchUrl(query);
-    
+    imgMedia.src = movie.image;
+    imgMedia.alt = movie.name;
+    divMedia.appendChild(imgMedia);
+    div1.appendChild(divMedia);
 
-    await fetch(url).then(resp => {
-        return resp.json();
-    }).then(movies => {
-        resultMovies = movies.results
-    });
-    return resultMovies;
+    document.querySelector('.movies__container').appendChild(div1);
+
 }
 
-function inputInModel(moviesList) {
+function addMovie(e) {
+    e.preventDefault();
+    let $movieName = document.querySelector('input[name="name"]').value
+    let $movieTrailerLink = document.querySelector('input[name="link"]').value
+    let $movieImageUrl = document.querySelector('input[name="imagem"]').value
+    let $movieReview = document.querySelector('input[name="review"]').value;
 
-    moviesList.forEach(movie => {
-        let liMovie = document.createElement('li');
-        liMovie.innerHTML = `<span>${movie.title}</span> <span>${movie.release_date} </span>`;
-        liMovie.dataset.movieId = movie.id;
-        liMovie.addEventListener('click', addClickClass);
-        $resultList.appendChild(liMovie);
-    })
-}
+    let inputs = Array.from(document.querySelectorAll('input[type="text"]'));
+    let errMsg = 'Preencha todos os campos!!';
 
-async function valideKey(e) {
-    if (e.key == 'Enter') {
-        $resultList.innerHTML = '';
-        inputInModel(await loadSearch(e.target.value));
+
+    if (inputs.every(el => el.value != '')) {
+        if(!$movieImageUrl.endsWith('.jpg') && !$movieImageUrl.endsWith('.png') && !$movieImageUrl.endsWith('.jfif') && !$movieImageUrl.endsWith('.gif')){
+            throwError('link da imagem é inválida');
+        }
+        if($movieTrailerLink.substr(0,12) != 'https://www.' && $movieTrailerLink.substr(0,16) != 'https://youtu.be' && $movieTrailerLink.substr(0,32) != 'https://www.youtube.com/watch?v='){
+            throwError('link do trailer é inválido');
+        }
+        if ($movieReview.substr(0, 12) != 'https://www.') {
+            throwError('link do review é inválido');
+        }
     }
-    if (e.key == 'Backspace') {
-        $resultList.innerHTML = '';
-        inputInModel(await loadSearch(e.target.value));
-    }
+    // let obj = createFavoriteMovie($movieName, $movieTrailerLink, $movieImageUrl, $movieReview);
+    // console.log(obj);
+    // showMovieOnPage(obj);
+    // resetClear();
+
 }
 
-function addClickClass(e) {
-    e.target.classList.toggle('list-item--clicked');
+function resetClear() {
+    document.querySelector('input[name="name"]').value = '';
+    document.querySelector('input[name="link"]').value = '';
+    document.querySelector('input[name="imagem"]').value = ''
+    document.querySelector('input[name="review"]').value = '';
+    document.querySelector('.modal-container').classList.remove('display--flex');
+    document.querySelector('.modal-container').classList.add('display--none');
+
 }
 
-function uploadMovies(e) {
-    let list = document.querySelectorAll('.list-item--clicked');
-    console.log(list);
+function showModal() {
+    document.querySelector('.modal-container').classList.remove('display--none');
+    document.querySelector('.modal-container').classList.add('display--flex');
+}
+
+function throwError (msg){
+    document.querySelector('#msg-error').innerHTML = msg;
 }
